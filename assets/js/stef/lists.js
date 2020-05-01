27cd5fea62c7ae390b1e1926deeb3e397be00497
stef.lauwaet <stef.lauwaet@student.howest.be>
2020-05-01 19:36:52 +0200

    Fix type (thx Robbe) + beers for the boys

--- assets/js/stef/lists.js
+++ assets/js/stef/lists.js
@@ -1,10 +1,11 @@
 let lists = {
 
 
-    animals: ['Dog','Cat','Fish','Cow','Sheep','Monkey','Alcapa', 'Hedgehog'],
+    animals: ['Dog','Cat','Fish','Cow','Sheep','Monkey','Alpaca', 'Hedgehog'],
     capitals: ['Amsterdam','Parijs','Brussel','Londen','Berlijn','Kopenhagen','Madrid', 'Rome'],
     food: ['Pizza', "Kebab", "Hamburger", "Cucumber", "Lasagna", "Mustard","Rice","Apple"],
     cars: ['Alfa Romeo', "Bugatti", "Citroën", "Deauwoo", "Exagon", "Ferrari", "Ginetta", "Honda"],
-    names: ["Astrid", "Bert", "Cédric", "Dante", "Els", "Frans","Geraldine","Henk"]
+    names: ["Astrid", "Bert", "Cédric", "Dante", "Els", "Frans","Geraldine","Henk"],
+    beers: ["Omer", "Karmeliet", "Rodenbach", "Jupiler", "Duvel", "Cara", "Stella", "Chimay"]
 
 }
